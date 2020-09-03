0e8e0eadc566844f68089812a5771c47420bd9e8
Martin Holy <martin.holy@coin-exchange.cz>
2020-09-03 23:56:13 +0200

    .

--- docker/run.sh
+++ docker/run.sh
@@ -21,7 +21,9 @@ declare -a l_travel_minutes=()
 ##         ##
 # pro prevod diakritiky v url
 function uriencode() {
-  jq -nr --arg v "$1" '$v|@uri'
+  local str="${1}"
+
+  jq -nr --arg v "${str}" '$v|@uri'
 }
 
 function strip_location() {
@@ -30,9 +32,9 @@ function strip_location() {
   
   if [[ $(echo "${loc_fullname}" | grep -c 'okres') -gt 0 ]]
   then
-    loc_stripped=$(echo "${loc_fullname}" | cut -f 1 -d ',')
+    loc_stripped=$(echo "${loc_fullname}" | cut -f 1 -d ',' | sed 's/ - /,/')
   else
-    loc_stripped=$(echo "${loc_fullname}" | cut -f 2- -d ',')
+    loc_stripped=$(echo "${loc_fullname}" | cut -f 2- -d ',' | sed 's/ - /,/')
     loc_stripped=${loc_stripped#?}
   fi
   
@@ -63,42 +65,48 @@ function find_minimum() {
   return
 }
 
-function db_adv_present() {
+function db_location_recid() {
   local hash="${1}"
-  local which_hash="`echo \"${2}\" | tr 'A-Z' 'a-z'`" # url or location
 
-  adv_id=`echo "SELECT MAX(id) FROM adv_list WHERE ${which_hash}_hash = '"${hash}"';" | sqlite3 "${C_DB_FILE}"`
-  if [ -z ${adv_id} ]; then return 1; fi # not a such record in the db
+  res=`echo "
+-- check if we have the location info for that location
+SELECT al.id
+  FROM adv_locations al
+ WHERE 1 = 1
+       AND al.location_hash = '${hash}'
+ LIMIT 1;
+" | sqlite3 "${C_DB_FILE}"`
+  if [ -z ${res} ]; then return 1; fi # not a such record in the db
 
-  echo "${adv_id}"
+  echo "${res}"
   return 0
 }
 
-function db_notification_present() {
-  local adv_id="${1}"
+function db_traveltime_recminimum() {
+  local loc_id="${1}"
 
-  notif_id=`echo "SELECT MAX(id) FROM adv_notifications WHERE id_adv_list = ${adv_id};" | sqlite3 "${C_DB_FILE}"`
+  res=`echo "
+-- check if we have the travel info for that location
+SELECT tt.minimum
+  FROM travel_locations tl
+  JOIN adv_locations al ON al.id = tl.id_adv_location
+  JOIN travel_times tt ON tt.id = tl.id_adv_location
+ WHERE 1 = 1
+       AND al.id = '${loc_id}'
+ LIMIT 1;
+" | sqlite3 "${C_DB_FILE}"`
+  if [ -z ${res} ]; then return 1; fi # not a such record in the db
 
-  if [ -z ${notif_id} ]; then return 1; fi # not a such record in the db
-
-  echo "${notif_id}"
-  return 0
-}
-
-function db_traveltime_present() {
-  local adv_id="${1}"
-
-  trav_id=`echo "SELECT MAX(id) FROM adv_traveltime WHERE id_adv_list = ${adv_id};" | sqlite3 "${C_DB_FILE}"`
-
-  if [ -z ${trav_id} ]; then return 1; fi # not a such record in the db
-
-  echo "${trav_id}"
+  echo "${res}"
   return 0
 }
 
 ##M   ##
 # Main #
 ##    ##
+# remove older tmp files
+rm -f /app/tmp/*
+
 # ziskej prehled inzeratu
 google-chrome --no-sandbox --headless --disable-gpu --dump-dom 'https://www.sreality.cz/hledani/prodej/domy/rodinne-domy,vily,chalupy,pamatky-jine,zemedelske-usedlosti/stredocesky-kraj?pois_in_place_distance=1.5&per_page=100&pois_in_place=1%7C2&navic=samostatny&stav=po-rekonstrukci,novostavby,dobry-stav,velmi-dobry-stav&plocha-od=90&plocha-do=10000000000&cena-od=0&cena-do=5000000&plocha-pozemku-od=0&plocha-pozemku-od=500&plocha-pozemku-do=2000&bez-aukce=1' > ${C_LIST_FILE} 2>/dev/null
 
@@ -124,6 +132,18 @@ do
       curl -sfkL 'https://idos.idnes.cz/vlakyautobusymhdvse/spojeni/vysledky/?date='${C_NEXT_TUESDAY}'&time=05:00&f='${loc_from}'&fc='${station_type}'&t='${loc_to}'&tc='${station_type} >> /app/tmp/idos-${idx}.dump 2>/dev/null
     done
   done
+
+  loc_rec_id=`db_location_recid "${sha_loc}"`
+  if [[ $? -eq 0 ]]; then
+    travel_minutes_minimum=`db_traveltime_recminimum "${loc_rec_id}"`
+    if [[ $? -ne 0 ]]; then
+      echo "Could not find the travel time information in DB."
+    else
+      echo "Travel time found in DB: ${travel_minutes_minimum}"
+    fi
+  else
+    echo "\"${loc}\" is NOT in DB."
+  fi
   
   IFS=$'\n'
   l_travel_times=(`cat /app/tmp/idos-${idx}.dump | grep -oiE 'Celkový čas.*(hod|min)' | cut -f 2 -d '>'`)
