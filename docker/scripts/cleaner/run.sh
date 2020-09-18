cfb749b7dba102275a8aef420eeed4564faa1ad1
Martin Holy @ amnesiac SRV <mh@amnesiac-srv>
2020-09-18 14:22:55 +0000

    fixed error with deleting travel time connections table

--- docker/scripts/cleaner/run.sh
+++ docker/scripts/cleaner/run.sh
@@ -9,17 +9,13 @@ declare -a l_records=(`echo "
 --
 SELECT acl.id, au.id, tl.id
   FROM adv_completion_list acl
-  JOIN adv_urls au         ON au.id = acl.id_adv_url
-  JOIN adv_locations al    ON al.id = acl.id_adv_location
-  JOIN travel_locations tl ON tl.id = acl.id_adv_location
-  JOIN travel_times tt     ON tt.id = tl.id_travel_time;
+  JOIN adv_urls au         ON au.id = acl.id_adv_url;
 " | sqlite3 ${C_DB_FILE}`)
 
 for rec in ${l_records[@]}
 do
   compl_id=`echo ${rec} | cut -f 1 -d '|'`
   url_id=`echo ${rec} | cut -f 2 -d '|'`
-  travloc_id=`echo ${rec} | cut -f 3 -d '|'`
   
   url=`echo "
 SELECT url
@@ -32,7 +28,6 @@ SELECT url
     echo "Removing:"
     echo -e "\tadv_completion_list id = ${compl_id}"
     echo -e "\tadv_urls id            = ${url_id}"
-    echo -e "\ttravel_locations id    = ${travloc_id}"
     echo -e "\turl                    = ${url}"
     echo "===================================="
   
@@ -40,10 +35,6 @@ SELECT url
 DELETE
   FROM adv_completion_list
  WHERE id = ${compl_id};
- 
-DELETE
-  FROM travel_locations
- WHERE id = ${travloc_id};
  
 DELETE
   FROM adv_urls
